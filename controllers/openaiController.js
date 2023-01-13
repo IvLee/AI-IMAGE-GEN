9a846dec1480a46a62f70eabf0f3b1e8e3b9fb35
Ivan Lee <ilee.4123@gmail.com>
2023-01-13 03:17:51 -0500

    Create video script AI

--- controllers/openaiController.js
+++ controllers/openaiController.js
@@ -38,4 +38,50 @@ const generateImage = async (req, res) => {
     }
 }
 
-module.exports = {generateImage};
+
+function generatePrompt(name, background, courseTopic, reason, target){
+    
+  return `Write a lesson welcome video script for ${target} about ${name} with a background in ${background} who is interested in ${courseTopic} because of ${reason} and the benefits of taking this course.
+  Display as a welcome video script`;
+
+}
+
+const generateVideoScript = async (req, res) => {
+    //const {name, background, courseTopic, reason} = req.body;
+    
+    try {
+        const completion = await openai.createCompletion({
+            model: "text-davinci-003",
+            prompt: generatePrompt(req.body.name,req.body.background,req.body.courseTopic,req.body.reason,req.body.target),
+            max_tokens: 70,
+            temperature: 0.6,
+            top_p: 1,
+            n: 1,
+            stream: false,
+            logprobs: null,
+          });
+
+          const videoScript = completion.data.choices[0].text;
+
+          console.log(videoScript);
+
+        res.status(200).json({
+            success: true,
+            data: videoScript
+        });
+    } catch (error) {
+        if (error.response) {
+            console.log(error.response.status);
+            console.log(error.response.data);
+          } else {
+            console.log(error.message);
+          }
+
+        res.status(400).json({
+            success: false,
+            error: 'Script could not be generated'
+        });
+    }
+}
+
+module.exports = {generateImage, generateVideoScript};
--- routes/openaiRoutes.js
+++ routes/openaiRoutes.js
@@ -1,8 +1,10 @@
 const express = require('express');
-const {generateImage} = require('../controllers/openaiController');
+const {generateImage, generateVideoScript} = require('../controllers/openaiController');
 const router = express.Router();
 
 router.post('/generateimage', generateImage);
+
+router.post('/generateVideoScript', generateVideoScript);
 
 module.exports = router;
 
